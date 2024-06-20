d9e1427c7ca9d56edfa327691525dd9e7df77e43
stubfx <14958556+stubfx@users.noreply.github.com>
2024-06-20 21:29:53 +0200

    dealing with treesitter installer

--- init.lua
+++ init.lua
@@ -59,7 +59,28 @@ require('lazy').setup({
   -- Other useful completion sources
   { 'hrsh7th/cmp-buffer' },
   { 'hrsh7th/cmp-path' },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+--  {
+--	  "nvim-treesitter/nvim-treesitter",
+--	  build = ":TSUpdate",
+--	  config = function () 
+--		  local configs = require("nvim-treesitter.configs")
+--
+--		  configs.setup({
+--			  -- yeah, don't do this.
+--			  -- ensure_installed = "all",
+--			  ensure_installed = {"javascript", "html", "python"},
+--			  -- especially on windows system, apparently it breaks? i still don't know why, possibly cmd
+--			  -- will investigate, but for the moment installing sync works.
+--			  sync_install = true,
+--			  highlight = { enable = true },
+--			  indent = { enable = true },  
+--		  })
+--	  end
+--  },
+-- {
+--    "nvim-treesitter/playground",
+--    dependencies = { "nvim-treesitter/nvim-treesitter" }  -- Ensures load order
+--  }
 })
 
 -- nvim-cmp setup
