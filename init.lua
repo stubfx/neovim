15d6f1a9e4118fd0739ed6f469e0cc7d69c29888
stubfx <14958556+stubfx@users.noreply.github.com>
2024-06-20 22:04:51 +0200

    add number and relativenumber

--- init.lua
+++ init.lua
@@ -105,6 +105,8 @@ cmp.setup({
     { name = 'path' },
   }
 })
+vim.wo.number = true
+vim.wo.relativenumber = true
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 --   vim.lsp.diagnostic.on_publish_diagnostics, {
 --     virtual_text = true,
