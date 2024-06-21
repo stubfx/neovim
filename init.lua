f024bf03cda224f6a2780b404c0cb8a51902010c
stubfx <14958556+stubfx@users.noreply.github.com>
2024-06-21 21:36:53 +0200

    forcing treesitter to use zig cause windows cl is giving me headaches.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- init.lua
-
+-- vim.fn.setenv("CC", "zig cc")
 -- Bootstrap lazy.nvim (if not already done)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -59,28 +59,30 @@ require('lazy').setup({
   -- Other useful completion sources
   { 'hrsh7th/cmp-buffer' },
   { 'hrsh7th/cmp-path' },
---  {
---	  "nvim-treesitter/nvim-treesitter",
---	  build = ":TSUpdate",
---	  config = function () 
---		  local configs = require("nvim-treesitter.configs")
---
---		  configs.setup({
---			  -- yeah, don't do this.
---			  -- ensure_installed = "all",
---			  ensure_installed = {"javascript", "html", "python"},
---			  -- especially on windows system, apparently it breaks? i still don't know why, possibly cmd
---			  -- will investigate, but for the moment installing sync works.
---			  sync_install = true,
---			  highlight = { enable = true },
---			  indent = { enable = true },  
---		  })
---	  end
---  },
--- {
---    "nvim-treesitter/playground",
---    dependencies = { "nvim-treesitter/nvim-treesitter" }  -- Ensures load order
---  }
+  {
+	  "nvim-treesitter/nvim-treesitter",
+	  build = ":TSUpdate",
+	  config = function () 
+		  local configs = require("nvim-treesitter.configs")
+		  local install = require("nvim-treesitter.install")
+		  install.compilers = { "zig"}
+
+		  configs.setup({
+			  -- yeah, don't do this.
+			  -- ensure_installed = "all",
+			  ensure_installed = {"lua", "javascript", "html", "python"},
+			  -- especially on windows system, apparently it breaks? i still don't know why, possibly cmd
+			  -- will investigate, but for the moment installing sync works.
+			  sync_install = false,
+			  highlight = { enable = true },
+			  indent = { enable = true },  
+		  })
+	  end
+  },
+ {
+    "nvim-treesitter/playground",
+    dependencies = { "nvim-treesitter/nvim-treesitter" }  -- Ensures load order
+  }
 })
 
 -- nvim-cmp setup
