dd303b9adc547ca3e1ad0d859a4f66331e578aa1
stubfx <14958556+stubfx@users.noreply.github.com>
2024-07-11 14:41:04 +0200

    Adding lualine and zenmode

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -3,5 +3,25 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+            require('lualine').setup {
+                options = {
+                    theme = "catppuccin"
+                    -- ... the rest of your lualine config
+                }
+            }
+        end
+    },
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
     }
 }
