69acb01c766f5b02d006f61c9417a92d4ffecfef
stubfx <14958556+stubfx@users.noreply.github.com>
2024-07-26 17:12:11 +0200

    minor fix gen.nvim

--- lua/plugin/gen.lua
+++ lua/plugin/gen.lua
@@ -22,7 +22,7 @@ return {
             display_mode = "split", -- The display mode. Can be "float" or "split" or "horizontal-split".
             show_prompt = false, -- Shows the prompt submitted to Ollama.
             show_model = false, -- Displays which model you are using at the beginning of your chat session.
-            no_auto_close = false, -- Never closes the window automatically.
+            no_auto_close = true, -- Never closes the window automatically.
             debug = false -- Prints errors and the command which is run.
         }
     }
