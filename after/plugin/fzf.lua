d31fa423ccc4794624d553eb336599bc97489f17
stubfx <14958556+stubfx@users.noreply.github.com>
2024-08-01 22:31:02 +0200

    minor fzf remappings in n and v mode for ease of use

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -3,5 +3,7 @@ local fzf_lua = require('fzf-lua')
 -- Keybindings
 vim.api.nvim_set_keymap('n', '<leader>pf', ':FzfLua files<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-p>', ':FzfLua git_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require("fzf-lua").grep_cword()<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require("fzf-lua").grep_cword()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>ps', ':lua require("fzf-lua").grep_visual()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require("fzf-lua").grep()<CR>', { noremap = true, silent = true })
 
