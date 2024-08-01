37e46f45cbe105731ebe680edfd769afa36de309
stubfx <14958556+stubfx@users.noreply.github.com>
2024-08-01 12:55:42 +0200

    replace telescope with fzf-lua

---
+++ after/plugin/fzf.lua
@@ -0,0 +1,7 @@
+local fzf_lua = require('fzf-lua')
+
+-- Keybindings
+vim.api.nvim_set_keymap('n', '<leader>pf', ':FzfLua files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-p>', ':FzfLua git_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ps', ':lua require("fzf-lua").grep_cword()<CR>', { noremap = true, silent = true })
+
