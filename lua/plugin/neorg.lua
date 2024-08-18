1d1e52ea527a0e25828d39557336dcd8a0d69733
stubfx <14958556+stubfx@users.noreply.github.com>
2024-08-18 12:46:52 +0200

    add neorg configuration for personal notes

---
+++ lua/plugin/neorg.lua
@@ -0,0 +1,35 @@
+return {
+    {
+      "nvim-neorg/neorg",
+      lazy = false,
+      version = "*",
+      config = function()
+        require("neorg").setup {
+          load = {
+            ["core.defaults"] = {},
+            ["core.concealer"] = {},
+            ["core.dirman"] = {
+              config = {
+                workspaces = {
+                  notes = "~/notes",
+                },
+                default_workspace = "notes",
+              },
+            },
+          },
+        }
+  
+        vim.wo.foldlevel = 99
+        vim.wo.conceallevel = 2
+      end
+    },
+    {
+        "dhruvasagar/vim-table-mode",
+        config = function ()
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = { 'markdown', 'norg' },
+                command = 'TableModeEnable',
+            })
+        end
+    }
+}
