866c1640c220c94fdec885e2ccc9b55df9768ca0
stubfx <14958556+stubfx@users.noreply.github.com>
2024-09-22 17:35:44 +0200

    working on color palette

--- lua/plugin/catppuccin.lua
+++ lua/plugin/catppuccin.lua
@@ -5,7 +5,7 @@ return {
         priority = 1000,
         config = function ()
             require('catppuccin').setup {
-                flavour = "auto", -- latte, frappe, macchiato, mocha
+                -- flavour = "auto", -- latte, frappe, macchiato, mocha
                 background = { -- :h background
                     light = "latte",
                     dark = "mocha",
@@ -36,7 +36,6 @@ return {
                     operators = {},
                     -- miscs = {}, -- Uncomment to turn off hard-coded styles
                 },
-                custom_highlights = {},
                 default_integrations = true,
                 integrations = {
                     cmp = true,
@@ -58,6 +57,37 @@ return {
                         peach = "#ff940a",
                     }
                 },
+                custom_highlights = {
+                    Normal = { fg = "#ffffff" },
+                    Comment = { fg = "#6c6f93" }, -- Grey
+                    Keyword = { fg = "#ff00ff", style = { "italic" } }, -- Vibrant pink
+                    Function = { fg = "#00ffff" }, -- Bright cyan
+                    Identifier = { fg = "#00aaff" }, -- Vibrant blue
+                    Statement = { fg = "#ff6600" }, -- Orange
+                    Type = { fg = "#ffff00" }, -- Yellow
+                    String = { fg = "#00e600" }, -- Bright green
+                    Number = { fg = "#00ccff" }, -- Light blue
+                    Constant = { fg = "#ff33cc" }, -- Pink
+                    Operator = { fg = "#cc00ff" }, -- Purple
+                    -- Header colors
+                    markdownH1 = { fg = "#ff00ff" }, -- Vibrant pink
+                    markdownH2 = { fg = "#00ffff" }, -- Bright cyan
+                    markdownH3 = { fg = "#00aaff" }, -- Vibrant blue
+                    markdownH4 = { fg = "#ff6600" }, -- Orange
+                    -- Code block
+                    markdownCode = { fg = "#cc00ff" }, -- Purple
+                    markdownCodeBlock = { fg = "#00ff00" }, -- Bright green
+                    -- Special colors
+                    Error = { fg = "#ff0000" }, -- Red
+                    Warning = { fg = "#ffcc00" }, -- Yellow-orange
+                    Info = { fg = "#0077ff" }, -- Bright blue
+                    Hint = { fg = "#00ffcc" }, -- Cyan-green
+                    -- Cursor line (optional if you want a specific color for cursor line)
+                    CursorLine = { },
+                    -- Popup menu
+                    Pmenu = { fg = "#ffffff" }, -- White text for menu
+                    PmenuSel = { fg = "#000000", bg = "#ff00ff" }, -- Black text on bright pink background
+                }
             }
         end
     }
